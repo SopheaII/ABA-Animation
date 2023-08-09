1eb489afcd233645fde394d6a51d3527a31d5ae9
SaoSophea <sophea.sao@wbfinance.com.kh>
2023-08-09 08:31:21 +0700

    Initial Commit

---
+++ ABAClone/ABACloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ABACloneApp.swift
+//  ABAClone
+//
+//  Created by Sao Sophea on 9/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ABACloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
